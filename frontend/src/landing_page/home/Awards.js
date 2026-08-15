a8b3164fddb5922587e329dd394a68c0b0e45a0c
Rutuja Khade <khaderutuja17@gmail.com>
2026-08-15 22:39:44 +0530

    Added style to home components

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,7 +2,46 @@ import React from 'react';
 
 function Awards() {
     return (
-        <h1>Awards</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6 p-5'>
+                    <img src='/images/largestBroker.svg'/>
+                </div>
+                <div className='col-6 p-5 mt-5'>
+                    <h1>Largest stock broker in India</h1>
+                    <p>2+ million TradeMind Clients contribute to over 15% of all volumes in India by trading and investing in:</p>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <ul>
+                        <li>
+                            <p>Futures and options</p>
+                        </li>
+                        <li>
+                            <p>Commodity derivatives</p>
+                        </li>
+                        <li>
+                            <p>Currency derivatives</p>
+                        </li>
+                    </ul>
+                        </div>
+                        <div className='col-6'>
+                            <ul>
+                        <li>
+                            <p>Stocks and IPOs</p>
+                        </li>
+                        <li>
+                            <p>Direct mutual funds</p>
+                        </li>
+                        <li>
+                            <p>Bonds and Govt. Securities</p>
+                        </li>
+                    </ul>
+                        </div>
+                    </div>
+                    <img src='images\pressLogos.png'style={{width:"90%"}}/>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Awards;
